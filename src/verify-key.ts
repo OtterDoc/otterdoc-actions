4a0788299fcc63adc1ede7b88c568f1f41e26d1b
Chris Coudron <coudron@gmail.com>
2023-06-13 13:17:44 -0400

    Add instructions to get key

--- src/verify-key.ts
+++ src/verify-key.ts
@@ -14,5 +14,16 @@ export async function VerifyOtterDocKey(key: string): Promise<boolean> {
     return true
   }
   core.warning('Could not verify key')
+  core.warning('Please make sure you have added your otterdoc key to your repo secrets')
+  core.warning('To add:')
+  core.warning('    - Go to your repo page')
+  core.warning('    - Click "settings"')
+  core.warning('    - Expand "secrets and variables" on the left hand side')
+  core.warning('    - Select "Actions" submenu')
+  core.warning('    - Select "Actions" submenu')
+  core.warning('    - Click the "New repository secret" button')
+  core.warning('    - Can be found at this url: https://github.com/<ORG>/<REPO>/settings/secrets/actions/new')
+  core.warning('    - Key name: "OTTERDOC_KEY"')
+  core.warning('    - Get your OtterDoc key at https://www.otterdoc.ai under your account')
   return false
 }
