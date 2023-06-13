056dd4e8b6cecbdaa78f94b1bb779077417f28a3
Chris Coudron <coudron@gmail.com>
2023-06-13 12:50:46 -0400

    Better logging for key checking.

--- src/verify-key.ts
+++ src/verify-key.ts
@@ -3,15 +3,16 @@ import axios from 'axios'
 
 export async function VerifyOtterDocKey(key: string): Promise<boolean> {
   const otterdocUrl = process.env.OTTERDOC_URL || 'https://www.otterdoc.ai'
-  const result = await axios.get(
-    otterdocUrl + `/api/verify-key?key=${key}`
-  )
+  const result = await axios
+    .get(otterdocUrl + `/api/verify-key?key=${key}`)
+    .catch(error => {
+      console.log('Error verify key:', error.response)
+    })
 
-  if (result.status === 200) {
+  if (result?.status === 200) {
     core.warning('Key is valid')
     return true
   }
-  core.warning('Key is invalid')
-  core.warning(`Verify status code: ${result.status}`)
+  core.warning('Could not verify key')
   return false
 }
